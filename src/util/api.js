3ab78465fe052c207658dfa6f49d2afd1f1e3050
damian <damian20lewandowski@gmail.com>
2018-02-15 19:31:25 +0100

    fixed http

--- src/util/api.js
+++ src/util/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiKey = "5ca569f958081bf33ad70ad616e40bd9";
 
 function getWeather(cityCountry) {
-  const uri = `https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast?units=metric&q=${cityCountry}&appid=${apiKey}`;
+  const uri = `https://crossorigin.me/https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${cityCountry}&appid=${apiKey}`;
   console.log(uri);
   const encodedURI = window.encodeURI(uri);
   
