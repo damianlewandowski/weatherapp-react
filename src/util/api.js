73cfdbaae2cc2b3ed6c6cc6082f58625f93a7b8a
Damian <damian20lewandowski@gmail.com>
2019-05-09 11:08:26 +0200

    Removed Readme and CORS proxy

--- src/util/api.js
+++ src/util/api.js
@@ -1,14 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiKey = "5ca569f958081bf33ad70ad616e40bd9";
 
 function getWeather(cityCountry) {
-  const uri = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${cityCountry}&appid=${apiKey}`;
+  const uri = `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${cityCountry}&appid=${apiKey}`;
   console.log(uri);
   const encodedURI = window.encodeURI(uri);
-  
-  return axios
-    .get(encodedURI)
+
+  return axios.get(encodedURI);
 }
 
-export {getWeather};
+export { getWeather };
